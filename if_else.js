6fca9647c50bff602a6db4dc6e6c982b37864ebf
toxi <xseven693@gmail.com>
2021-04-13 12:29:51 +0300

    update if_else

--- if_else.js
+++ if_else.js
@@ -1,8 +1,20 @@
-function testIf(a, b) {
+function testIf_1(a, b) {
     var x;
     if(a > b){
         x = a + b;
     }else{
+        x = a * b;
+    }
+    return x;
+}
+
+function testIf2(a, b) {
+    var x;
+    if (a < b){
+        x = a + b;
+    } else if (a > b){
+        x = a - b;
+    } else {
         x = a * b;
     }
     return x;
